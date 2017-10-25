22c2a32b6fe064407906aaa0f60234187ce6660f
Nick Chen <nicholas_chan82@hotmail.com>
2017-10-25 14:25:21 +0800

    minor fix

--- src/data/account/Auth.js
+++ src/data/account/Auth.js
@@ -2,19 +2,21 @@ import axios from 'axios';
 import endpoints, { cid, secret } from '../Endpoint';
 
 export const getTokenAsync = async (email, password)=>{
-    return axios({method: 'post', 
-                 url: endpoints.OAUTH,
-                 headers: {'Content-Type': 'application/json'},
-                 data: {
-                    username: email,
-                    password,
-                    grant_type: "password",
-                    client_id: cid,
-                    client_secret: secret,
-                    scope: ""
-                 }}).then(function(response){
-                    return response.data.data;
-                 }).catch(function(err){
-                    return null;
-                 }); 
-}
+    try{
+        const response = await axios({method: 'post', 
+                                    url: endpoints.OAUTH,
+                                    headers: {'Content-Type': 'application/json'},
+                                    data: {
+                                        username: email,
+                                        password,
+                                        grant_type: "password",
+                                        client_id: cid,
+                                        client_secret: secret,
+                                        scope: ""
+                                    }})
+        return response.data.data;
+                
+    }catch(e){
+        return null
+    }
+}            
