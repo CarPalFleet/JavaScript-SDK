d7e8fb9baf5ed2d792b08d62f1d9ce72340cd2c5
Nick Chen <nicholas_chan82@hotmail.com>
2017-10-31 15:51:41 +0800

    added getDriverLegsAsync function to SDK

--- src/data/account/__test__/account.test.js
+++ src/data/account/__test__/account.test.js
@@ -1,4 +1,4 @@
-import { resetPasswordAsync, getMyJobsAsync } from '../Account';
+import { resetPasswordAsync, getDriverJobsAsync, getDriverLegsAsync } from '../Account';
 import { getTokenAsync } from '../Auth';
 
 test('Test for reset password', async ()=>{    
@@ -10,8 +10,17 @@ test('Test for reset password', async ()=>{
 test('Test for getting my jobs.', async ()=>{    
     const result = getTokenAsync('demo@carpal.me', 'carpaldemo');
     const token = await result;
-    const response = getMyJobsAsync(1, token.access_token, '2017-11-01');
+    const response = getDriverJobsAsync(1, token.access_token, '2017-11-01');
     const myJobs = await response;
     expect(myJobs instanceof Array).toBe(true);
 })
+
+test('Test for getting my legs.', async ()=>{    
+    const result = getTokenAsync('demo@carpal.me', 'carpaldemo');
+    const token = await result;
+    const response = getDriverLegsAsync(1, token.access_token, '2017-11-01');
+    const myLegs = await response;
+    expect(myLegs instanceof Array).toBe(true);
+})
+
 
