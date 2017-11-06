a2c941ff31d8f350cd44f3cc65390ed51c230a31
Dede <winsandymyint@gmail.com>
2017-11-06 18:20:51 +0800

    Update all response into camel Case

---
+++ src/data/CamelCase.js
@@ -0,0 +1,19 @@
+import _ from 'lodash';
+
+const CamelCase = (data) => {
+  let camelCaseObject = {};
+  _.forEach(
+    data,
+    function(value, key) {
+      /* Check whether the value is Object OR Array */
+      if (_.isPlainObject(value) || _.isArray(value)) {
+        /* Update the keys of value(Object/Array) recursively */
+        value = CamelCase(value);
+      }
+      camelCaseObject[_.camelCase(key)] = value;
+    }
+  )
+  return camelCaseObject;
+}
+
+export default CamelCase;
--- src/data/customer/Customer.js
+++ src/data/customer/Customer.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 import endpoints from '../Endpoint';
+import toCamelCase from '../CamelCase';
 
-export const createNewCustomerAsync = async ({email, password, firstName, lastName, phone, 
+export const createNewCustomerAsync = async ({email, password, firstName, lastName, phone,
                                          birthday, identityId, coName, coPhone, coVatNo})=>{
     try{
-        const response = await axios({method: 'post', 
+        const response = await axios({method: 'post',
                                       url: endpoints.NEW_CUSTOMER,
                                       headers: {'Content-Type': 'application/json'},
-                                      data: {                                        
+                                      data: {
                                         email,
                                         password,
                                         first_name: firstName,
@@ -17,9 +18,9 @@ export const createNewCustomerAsync = async ({email, password, firstName, lastNa
                                         birthday,
                                         company_name: coName,
                                         company_phone: coPhone,
-                                        company_vat_number: coVatNo                                        
+                                        company_vat_number: coVatNo
                                       }})
-        return response.data.data;     
+        return toCamelCase(response.data.data);
     }catch(e){
         return Promise.reject({statusCode: e.response.status, statusText: e.response.statusText});
     }
