8de5417be0a1601b8d337cd18696110a9966819f
Dede <winsandymyint@gmail.com>
2017-11-06 18:20:51 +0800

    Update all response into camel Case

--- src/data/account/Account.js
+++ src/data/account/Account.js
@@ -1,39 +1,39 @@
 import axios from 'axios';
 import endpoints from '../Endpoint';
+import toCamelCase from '../CamelCase';
 
 export const resetPasswordAsync = async (email)=>{
     try{
-        const response = await axios({method: 'post', 
+        const response = await axios({method: 'post',
                                       url: endpoints.PASSWORD_RESET,
                                       headers: {'Content-Type': 'application/json'},
                                       data: {
                                         email
                                       }})
-        return response.data;
-                
+        return toCamelCase(response.data);
     }catch(e){
         console.log(e);
         return Promise.reject({statusCode: e.response.status, statusText: e.response.statusText});
     }
-}  
+}
 
 export const getDriverJobsAsync = async (id, token, date)=>{
     try{
-        const response = await axios({method: 'get', 
+        const response = await axios({method: 'get',
                                       url: endpoints.MY_JOBS.replace('{1}', id).replace('{2}', date),
                                       headers: {'Authorization': token}});
-        return response.data.data;
+        return toCamelCase(response.data.data);
     }catch(e){
         return Promise.reject({statusCode: e.response.status, statusText: e.response.statusText});
-    }    
+    }
 }
 
 export const getDriverLegsAsync = async(id, token, date)=>{
     try{
-        const response = await axios({method: 'get', 
+        const response = await axios({method: 'get',
                                       url: endpoints.MY_LEGS.replace('{1}', id).replace('{2}', date),
                                       headers: {'Authorization': token}});
-        return response.data.data;
+        return toCamelCase(response.data.data);
     }catch(e){
         return Promise.reject({statusCode: e.response.status, statusText: e.response.statusText});
     }
--- src/data/public/Country.js
+++ src/data/public/Country.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import endpoints from '../Endpoint';
+import toCamelCase from '../CamelCase';
 
 export const getCountriesAsync = async ()=>{
     try{
         const response = await axios.get(endpoints.COUNTRIES)
-        return response.data.data;               
+        return toCamelCase(response.data.data);
     }catch(e){
         return Promise.reject({statusCode: e.response.status, statusText: e.response.statusText});
     }
-}            
+}
