1e04e3b47554caa55fb48b061bcad61d8f3d42c2
Nick Chen <nicholas_chan82@hotmail.com>
2017-11-07 17:12:25 +0800

    CON-110 #fixed the test cases for order detail

--- src/data/customer/__test__/Config.js
+++ src/data/customer/__test__/Config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-  email: 'demo@carpal.me',
-  password: 'carpaldemo',
+  email: 'vader1509606479983@carpal.me',
+  password: 'darthvader',
   clientId: '2',
   token: 'jWu9Qz4L3ha4SRgM5J6oBzAljg6f9zDzs2hIIIUh',
   domain: 'ezbuy.sg',
