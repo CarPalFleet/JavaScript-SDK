a207e1c9dc73ac5ccf7b082844aa4743024d8f31
Dede <winsandymyint@gmail.com>
2017-11-08 00:39:32 +0800

    Add unit testing for empty params/invalid domain Try/Catch for 403 error code

--- src/data/public/Setting.js
+++ src/data/public/Setting.js
@@ -5,10 +5,15 @@ import toCamelCase from '../CamelCase';
 export const getCustomerPublicProfileSettingsAsync = async (domain) => {
     try{
         const response = await axios({method: 'get',
-                                      url: endpoints.WHITE_LABEL.replace('{1}', domain)});
+                                      url: endpoints.TRANSACTION_GROUP_SETTING.replace('{1}', domain)});
         return toCamelCase(response.data);
-    }catch(e){
-        console.log("ERROR", e.response.status);
-        return Promise.reject({statusCode: e.response.status, statusText: e.response.statusText});
+    } catch(e) {
+        try {
+          return Promise.reject({statusCode: e.response.status, statusText: e.response.statusText});
+        } catch (e) {
+          /* Catch error of e.response
+          That will be undefined when status code is 403 Forbidden */
+          return Promise.reject({statusCode: 403, statusText: 'Forbidden'});
+        }
     }
 }
--- src/data/public/__test__/setting.test.js
+++ src/data/public/__test__/setting.test.js
@@ -3,18 +3,18 @@ import { getCustomerPublicProfileSettingsAsync } from '../Setting';
 import CONFIG from './Config';
 import _ from 'lodash';
 
-test('Test for getting WhiteLabel for customer', async () =>{
+test('Test for getting WhiteLabel for public', async () => {
     const response = getCustomerPublicProfileSettingsAsync(CONFIG.domain);
     const whiteLabel = await response;
     expect(_.findKey(whiteLabel.data, 'transactionGroupAssets')).toBeTruthy();
 })
 
-// test('Test for getting WhiteLabel for public with invalid domain', async()=>{
-//     const response = getCustomerPublicProfileSettingsAsync(CONFIG.invalidDomain);
-//     const whiteLabel = await response;
-//     expect(whiteLabel).rejects.toHaveProperty('statusCode', 404);
-//     // const aa = expect(whiteLabel).rejects;
-//     // console.log("REJECT", aa);
-//     // expect(whiteLabel).rejects.toHaveProperty('statusCode', 404);
-//     // expect(whiteLabel).rejects.toHaveProperty('Code', 'NotFoundError');
-// })
+test('Test for getting WhiteLabel for public with invalid domain', async () => {
+    const response = getCustomerPublicProfileSettingsAsync(CONFIG.invalidDomain);
+    await expect(response).rejects.toHaveProperty('statusCode', 404);
+})
+
+test('Test for getting WhiteLabel for public with empty string', async () => {
+    const response = getCustomerPublicProfileSettingsAsync(CONFIG.emptyString);
+    await expect(response).rejects.toHaveProperty('statusCode', 403);
+})
