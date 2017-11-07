5fe71602a7b1f50cbd7387290b7587b5fef47b9c
Dede <winsandymyint@gmail.com>
2017-11-07 17:01:43 +0800

    validate response object with lodash.findKey

--- src/data/public/__test__/setting.test.js
+++ src/data/public/__test__/setting.test.js
@@ -1,16 +1,18 @@
 import { getTokenAsync } from '../../account/Auth';
 import { getWhiteLabelAsync } from '../Setting';
 import CONFIG from './Config';
+import _ from 'lodash';
 
 test('Test for getting WhiteLabel for customer', async () =>{
     const response = getWhiteLabelAsync(CONFIG.domain);
     const whiteLabel = await response;
-    expect(whiteLabel instanceof Object).toBe(true);
+    expect(_.findKey(whiteLabel.data, 'transactionGroupAssets')).toBeTruthy();
 })
 
 // test('Test for getting WhiteLabel for public with invalid domain', async()=>{
 //     const response = getWhiteLabelAsync(CONFIG.invalidDomain);
 //     const whiteLabel = await response;
-//     // expect(whiteLabel).rejects.toHaveProperty('statusCode', 404);
+//     console.log("REJECT", whiteLabel);
+//     expect(whiteLabel).rejects.toHaveProperty('statusCode', 404);
 //     // expect(whiteLabel).rejects.toHaveProperty('Code', 'NotFoundError');
 // })
