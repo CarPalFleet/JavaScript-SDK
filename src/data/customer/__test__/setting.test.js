2e3b12b53816e9ebd10bea5c3777a334a19a5d40
Dede <winsandymyint@gmail.com>
2017-11-07 15:38:10 +0800

    Validate the respone is array or not

--- src/data/customer/__test__/setting.test.js
+++ src/data/customer/__test__/setting.test.js
@@ -5,6 +5,7 @@ import CONFIG from './Config';
 test('Test for getting WhiteLabel for customer', async () =>{
     const result = getTokenAsync(CONFIG.email, CONFIG.password, CONFIG.clientId, CONFIG.token);
     const token = await result;
+    console.log(token);
     const response = getWhiteLabelAsync(CONFIG.domain, token.accessToken);
     const whiteLabel = await response;
     expect(whiteLabel instanceof Object).toBe(true);
