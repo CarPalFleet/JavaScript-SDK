aee654d0329b5f198643646323cc01732da6439c
Dede <winsandymyint@gmail.com>
2017-11-07 13:51:28 +0800

    Update README for WhiteLabel Move setting.js under Customer

--- src/data/customer/__test__/Config.js
+++ src/data/customer/__test__/Config.js
@@ -2,7 +2,8 @@ const CONFIG = {
   EMAIL: 'demo@carpal.me',
   PASSWORD: 'carpaldemo',
   CLIENT_ID: '2',
-  TOKEN: 'jWu9Qz4L3ha4SRgM5J6oBzAljg6f9zDzs2hIIIUh'
+  TOKEN: 'jWu9Qz4L3ha4SRgM5J6oBzAljg6f9zDzs2hIIIUh',
+  DOMAIN: 'carpal.me'
 }
 
 export default CONFIG;
--- src/data/setting/__test__/Config.js
+++
@@ -1,9 +0,0 @@
-const CONFIG = {
-  EMAIL: 'demo@carpal.me',
-  PASSWORD: 'carpaldemo',
-  CLIENT_ID: '2',
-  TOKEN: 'jWu9Qz4L3ha4SRgM5J6oBzAljg6f9zDzs2hIIIUh',
-  DOMAIN: 'carpal.me'
-}
-
-export default CONFIG;
