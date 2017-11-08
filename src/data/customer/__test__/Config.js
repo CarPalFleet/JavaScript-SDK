5fdb5d31fbb4d083fa0a7e360e83573396fbe30f
Dede <winsandymyint@gmail.com>
2017-11-08 15:47:15 +0800

    Create Dummy Data for order

--- src/data/customer/__test__/Config.js
+++ src/data/customer/__test__/Config.js
@@ -5,7 +5,22 @@ const CONFIG = {
   token: 'jWu9Qz4L3ha4SRgM5J6oBzAljg6f9zDzs2hIIIUh',
   domain: 'ezbuy.sg',
   emptyString: '',
-  invalidDomain: 'aa.com'
+  invalidDomain: 'aa.com',
+  identityId: 1,
+  pickupDate: '2017-11-06',
+  startPickupDate: '2017-11-06',
+  endPickupDate: '2017-11-09',
+  orderOptionIds: [],
+  driverId: 2,
+  productTypeIds: [],
+  statusIds: [],
+  fields: [],
+  page: 1,
+  limit: 20,
+  promotionIds: [],
+  showCustomerOrderNumber: true,
+  showOrderOptions: false,
+  transactionGroupIds: []
 }
 
 export default CONFIG;
