91d83e8d8dc0b3a0af2c5d73f010eaf2bce20981
Nick Chen <nicholas_chan82@hotmail.com>
2017-11-08 17:32:44 +0800

    fix test cases

--- src/data/account/__test__/account.test.js
+++ src/data/account/__test__/account.test.js
@@ -1,12 +1,18 @@
-import { resetPasswordAsync, getDriverJobsAsync, getDriverLegsAsync } from '../Account';
+import { resetPasswordRequestAsync, resetPasswordAsync, getDriverJobsAsync, getDriverLegsAsync } from '../Account';
 import { getTokenAsync } from '../Auth';
 import CONFIG from './Config';
 
-test('Test for reset password', async ()=>{
-    const response = resetPasswordAsync(CONFIG.email);
-    const result = await result;
-    expect(result).not.toBeNull();
+test('Test for reset password request', async ()=>{
+    const response = resetPasswordRequestAsync(CONFIG.email);
+    const result = await response;
+    expect(result).toBe(true);
 })
+
+// test('Test for reset password', async ()=>{
+//     const response = resetPasswordAsync('6c328787ae0969bbde11be74ebbdcf1ba8d411d79502bb9bcc69729f5385370e', CONFIG.email, '123456', '123456');
+//     const result = await result;
+//     expect(result).toBe(true);
+// })
 
 test('Test for getting my jobs.', async ()=>{
     const result = getTokenAsync(CONFIG.email, CONFIG.password, CONFIG.clientId, CONFIG.token);
