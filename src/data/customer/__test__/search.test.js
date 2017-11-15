d22462b6905e41a3537b7357b340cee363d411e4
Nick Chen <nicholas_chan82@hotmail.com>
2017-11-15 10:40:00 +0800

    added async function and unit testing for notifications

---
+++ src/data/customer/__test__/search.test.js
@@ -0,0 +1,12 @@
+import { createNewCustomerAsync, createNewDriverAsync } from '../Customer';
+import { searchAsync } from '../Search';
+import { getTokenAsync } from '../../account/Auth';
+import CONFIG from './Config';
+
+test('Testing for search function', async () =>{
+    const result = getTokenAsync(CONFIG.email, CONFIG.password, CONFIG.clientId, CONFIG.token);
+    const token = await result;
+
+    const response = await searchAsync('test 123', 'customer, driver', true, token.accessToken);
+    expect('jobs' in response).toBe(true);
+})
