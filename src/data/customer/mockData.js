cee70e9daaa59e8fc59a3ed57a385198f62e86ab
Dede <winsandymyint@gmail.com>
2017-11-20 14:24:16 +0800

    Add MockupData for getCustomerDriversAsync and getOrdersWithFilterAsync

---
+++ src/data/customer/mockData.js
@@ -0,0 +1,438 @@
+export default {
+  "orders": [
+    {
+      "1": { // Assume that driver resources ID 1 is In-house
+        "orderCount": 1,
+        "ordersStatusName": "Jobs pending payment",
+        "data": {
+          "5345711": {
+            "id": 5345711,
+            "pickup_date": "2017-10-23",
+            "date_time_started": "0000-00-00 00:00:00",
+            "date_time_finished": "0000-00-00 00:00:00",
+            "Orders_status_Id": 1,
+            "type_id": 1,
+            "details_id": 53481,
+            "driver_id": 10157,
+            "user_id": 23031,
+            "location": {
+              "lat": 1222.3343,
+              "lng": 2444.223
+            },
+            "Orders_products_types_Id": 3,
+            "created_at": "2017-10-23 01:52:02"
+          },
+        }
+      }
+    },
+    {
+      "2": { // Assume that driver resources ID 1 is In-house
+        "orderCount": 2,
+        "ordersStatusName": "Dispatching jobs",
+        "data": {
+          "5345722": {
+            "id": 5345722,
+            "pickup_date": "2017-10-23",
+            "date_time_started": "0000-00-00 00:00:00",
+            "date_time_finished": "0000-00-00 00:00:00",
+            "Orders_status_Id": 2,
+            "type_id": 1,
+            "details_id": 53481,
+            "driver_id": 10157,
+            "user_id": 23031,
+            "location": {
+              "lat": 1222.3343,
+              "lng": 2444.223
+            },
+            "Orders_products_types_Id": 3,
+            "created_at": "2017-10-23 01:52:02"
+          },
+          "5407422": {
+            "id": 5407422,
+            "pickup_date": "2017-10-27",
+            "date_time_started": "2017-10-27 09:47:16",
+            "date_time_finished": "0000-00-00 00:00:00",
+            "Orders_status_Id": 2,
+            "type_id": 1,
+            "details_id": 54098,
+            "driver_id": 10136,
+            "user_id": 23031,
+            "location": {
+              "lat": 1222.3343,
+              "lng": 2444.223
+            },
+            "Orders_products_types_Id": 3,
+            "created_at": "2017-10-27 01:44:49"
+          },
+        }
+      },
+    },
+    {
+      "4": { // Assume that driver resources ID 1 is In-house
+        "orderCount": 1,
+        "ordersStatusName": "Cancelled jobs",
+        "data": {
+          "5342225744": {
+            "id": 5342225744,
+            "pickup_date": "2017-10-23",
+            "date_time_started": "0000-00-00 00:00:00",
+            "date_time_finished": "0000-00-00 00:00:00",
+            "Orders_status_Id": 4,
+            "type_id": 1,
+            "details_id": 53481,
+            "driver_id": 10157,
+            "user_id": 23031,
+            "location": {
+              "lat": 1222.3343,
+              "lng": 2444.223
+            },
+            "Orders_products_types_Id": 3,
+            "created_at": "2017-10-23 01:52:02"
+          }
+        }
+      },
+    },
+    {
+      "5": { // Assume that driver resources ID 1 is In-house
+        "orderCount": 2,
+        "ordersStatusName": "Jobs en-route",
+        "data": {
+          "5345711155": {
+            "id": 5345711155,
+            "pickup_date": "2017-10-23",
+            "date_time_started": "0000-00-00 00:00:00",
+            "date_time_finished": "0000-00-00 00:00:00",
+            "Orders_status_Id": 5,
+            "type_id": 1,
+            "details_id": 53481,
+            "driver_id": 10157,
+            "user_id": 23031,
+            "location": {
+              "lat": 1222.3343,
+              "lng": 2444.223
+            },
+            "Orders_products_types_Id": 3,
+            "created_at": "2017-10-23 01:52:02"
+          },
+          "5407422255": {
+            "id": 5407422255,
+            "pickup_date": "2017-10-27",
+            "date_time_started": "2017-10-27 09:47:16",
+            "date_time_finished": "0000-00-00 00:00:00",
+            "Orders_status_Id": 5,
+            "type_id": 1,
+            "details_id": 54098,
+            "driver_id": 10136,
+            "user_id": 23031,
+            "location": {
+              "lat": 1222.3343,
+              "lng": 2444.223
+            },
+            "Orders_products_types_Id": 3,
+            "created_at": "2017-10-27 01:44:49"
+          },
+        }
+      },
+    },
+    {
+      "6": { // Assume that driver resources ID 1 is In-house
+        "orderCount": 2,
+        "ordersStatusName": "Completed jobs",
+        "data": {
+          "5345711166": {
+            "id": 5345711166,
+            "pickup_date": "2017-10-23",
+            "date_time_started": "0000-00-00 00:00:00",
+            "date_time_finished": "0000-00-00 00:00:00",
+            "Orders_status_Id": 6,
+            "type_id": 1,
+            "details_id": 53481,
+            "driver_id": 10157,
+            "user_id": 23031,
+            "location": {
+              "lat": 1222.3343,
+              "lng": 2444.223
+            },
+            "Orders_products_types_Id": 3,
+            "created_at": "2017-10-23 01:52:02"
+          },
+          "5407422266": {
+            "id": 5407422266,
+            "pickup_date": "2017-10-27",
+            "date_time_started": "2017-10-27 09:47:16",
+            "date_time_finished": "0000-00-00 00:00:00",
+            "Orders_status_Id": 6,
+            "type_id": 1,
+            "details_id": 54098,
+            "driver_id": 10136,
+            "user_id": 23031,
+            "location": {
+              "lat": 1222.3343,
+              "lng": 2444.223
+            },
+            "Orders_products_types_Id": 3,
+            "created_at": "2017-10-27 01:44:49"
+          },
+        }
+      },
+    },
+    {
+      "8": { // Assume that driver resources ID 1 is In-house
+        "orderCount": 2,
+        "ordersStatusName": "Completed jobs",
+        "data": {
+          "534571116688": {
+            "id": 534571116688,
+            "pickup_date": "2017-10-23",
+            "date_time_started": "0000-00-00 00:00:00",
+            "date_time_finished": "0000-00-00 00:00:00",
+            "Orders_status_Id": 8,
+            "type_id": 1,
+            "details_id": 53481,
+            "driver_id": 10157,
+            "user_id": 23031,
+            "location": {
+              "lat": 1222.3343,
+              "lng": 2444.223
+            },
+            "Orders_products_types_Id": 3,
+            "created_at": "2017-10-23 01:52:02"
+          },
+          "540742226688": {
+            "id": 540742226688,
+            "pickup_date": "2017-10-27",
+            "date_time_started": "2017-10-27 09:47:16",
+            "date_time_finished": "0000-00-00 00:00:00",
+            "Orders_status_Id": 8,
+            "type_id": 1,
+            "details_id": 54098,
+            "driver_id": 10136,
+            "user_id": 23031,
+            "location": {
+              "lat": 1222.3343,
+              "lng": 2444.223
+            },
+            "Orders_products_types_Id": 3,
+            "created_at": "2017-10-27 01:44:49"
+          }
+        }
+      },
+    },
+    {
+      "9": { // Assume that driver resources ID 1 is In-house
+        "orderCount": 2,
+        "ordersStatusName": "Delayed Jobs",
+        "data": {
+          "534571116699": {
+            "id": 534571116699,
+            "pickup_date": "2017-10-23",
+            "date_time_started": "0000-00-00 00:00:00",
+            "date_time_finished": "0000-00-00 00:00:00",
+            "Orders_status_Id": 9,
+            "type_id": 1,
+            "details_id": 53481,
+            "driver_id": 10157,
+            "user_id": 23031,
+            "location": {
+              "lat": 1222.3343,
+              "lng": 2444.223
+            },
+            "Orders_products_types_Id": 3,
+            "created_at": "2017-10-23 01:52:02"
+          },
+          "540742226699": {
+            "id": 540742226699,
+            "pickup_date": "2017-10-27",
+            "date_time_started": "2017-10-27 09:47:16",
+            "date_time_finished": "0000-00-00 00:00:00",
+            "Orders_status_Id": 9,
+            "type_id": 1,
+            "details_id": 54098,
+            "driver_id": 10136,
+            "user_id": 23031,
+            "location": {
+              "lat": 1222.3343,
+              "lng": 2444.223
+            },
+            "Orders_products_types_Id": 3,
+            "created_at": "2017-10-27 01:44:49"
+          },
+        }
+      }
+    }
+  ],
+  "drivers": [
+    {
+      "1": { // Assume that driver resources ID 1 is In-house
+        "driverCount": 2,
+        "resourcesStatusName": "In-house",
+        "data": {
+          "10157": {
+            "id": 10157,
+            "Drivers_status_Id": 3,
+            "Drivers_products_types_Id": null,
+            "status_name": "Inactive drivers",
+            "product_name": null,
+            "Vehicle_type_Id": null,
+            "vehicle_model": null,
+            "vehicle_brand": null,
+            "vehicle_license_plate": null,
+            "vehicle_model_year": null,
+            "vehicle_type": null,
+            "first_name": "Dede",
+            "last_name": "Driver",
+            "email": "driver@carpal.me",
+            "phone": "+6587495944",
+            "location": {
+              "lat": 1222.3343,
+              "lng": 2444.223
+            },
+            "product_types": [
+              {
+                "id": 3,
+                "name": "CarPal Fleet"
+              }
+            ],
+            "driver_resources": {
+              "value": 1,
+              "label": "In-house"
+            },
+            "transaction_groups": [
+              {
+                "id": 123,
+                "name": "Dede Fleet Customer"
+              }
+            ]
+          },
+          "10158": {
+            "id": 10158,
+            "Drivers_status_Id": 3,
+            "Drivers_products_types_Id": null,
+            "status_name": "Inactive drivers",
+            "product_name": null,
+            "Vehicle_type_Id": null,
+            "vehicle_model": null,
+            "vehicle_brand": null,
+            "vehicle_license_plate": null,
+            "vehicle_model_year": null,
+            "vehicle_type": null,
+            "first_name": "Dede",
+            "last_name": "Driver",
+            "email": "driver@carpal.me",
+            "phone": "+6587495944",
+            "location": {
+              "lat": 1222.3343,
+              "lng": 2444.223
+            },
+            "product_types": [
+              {
+                "id": 3,
+                "name": "CarPal Fleet"
+              }
+            ],
+            "driver_resources": {
+              "value": 1,
+              "label": "In-house"
+            },
+            "transaction_groups": [
+              {
+                "id": 123,
+                "name": "Service Providers"
+              }
+            ]
+          }
+        }
+      },
+    },
+    {
+      "2": { // Assume that driver resources ID 2 is Public
+        "driverCount": 1,
+        "resourcesStatusName": "Public",
+        "data": {
+          "10159": {
+            "id": 10159,
+            "Drivers_status_Id": 2,
+            "Drivers_products_types_Id": null,
+            "status_name": "Active drivers",
+            "product_name": null,
+            "Vehicle_type_Id": null,
+            "vehicle_model": null,
+            "vehicle_brand": null,
+            "vehicle_license_plate": null,
+            "vehicle_model_year": null,
+            "vehicle_type": null,
+            "first_name": "Dede",
+            "last_name": "Driver1",
+            "email": "dede.driver1@carpal.me",
+            "phone": "+6587495944",
+            "location": {
+              "lat": 1222.3343,
+              "lng": 2444.223
+            },
+            "product_types": [
+              {
+                "id": 3,
+                "name": "CarPal Fleet"
+              }
+            ],
+            "driver_resources": {
+              "value": 1,
+              "label": "Public"
+            },
+            "transaction_groups": [
+              {
+                "id": 123,
+                "name": "Dede Fleet Customer",
+              }
+            ]
+          }
+        }
+      }
+    },
+    {
+      "3": { // Assume that driver resources ID 3 is Service Providers
+        "driverCount": 1,
+        "resourcesStatusName": "Service Providers",
+        "data": {
+          "10160": {
+            "id": 10160,
+            "Drivers_status_Id": 3,
+            "Drivers_products_types_Id": null,
+            "status_name": "Inactive drivers",
+            "product_name": null,
+            "Vehicle_type_Id": null,
+            "vehicle_model": null,
+            "vehicle_brand": null,
+            "vehicle_license_plate": null,
+            "vehicle_model_year": null,
+            "vehicle_type": null,
+            "first_name": "Dede",
+            "last_name": "Driver",
+            "email": "driver@carpal.me",
+            "phone": "+6587495944",
+            "location": {
+              "lat": 1222.3343,
+              "lng": 2444.223
+            },
+            "product_types": [
+              {
+                "id": 3,
+                "name": "CarPal Fleet"
+              }
+            ],
+            "driver_resources": {
+              "value": 1,
+              "label": "Service Providers"
+            },
+            "transaction_groups": [
+              {
+                "id": 123,
+                "name": "Service Providers"
+              }
+            ]
+          }
+        }
+      }
+    }
+  ]
+}
