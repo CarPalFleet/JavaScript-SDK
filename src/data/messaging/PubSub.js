c2fc2229e3f3c316e9f64052b73e9053bb8bcd82
Nick Chen <nicholas_chan82@hotmail.com>
2017-11-29 14:57:49 +0800

    Changed PubSub class to closure. Thanks jest...

--- src/data/messaging/PubSub.js
+++ src/data/messaging/PubSub.js
@@ -1,18 +1,42 @@
 import * as Ably from 'ably';
 
-export default class PubSub{
-    constructor(key){
-        this.client = new Ably.Realtime(key);
-    }
+// export default class PubSub{
+//     constructor(key){
+//         this.client = new Ably.Realtime(key);
+//         console.log(this.client);
+//     }
 
-    publish(channel, event, message){
-        const chan = this.client.channels.get(channel);
-        chan.publish(event, message);
-    }
+//     publish(channel, event, message){
+//         const chan = this.client.channels.get(channel);
+//         chan.publish(event, message);
+//     }
 
-    subscribe(channel, event, callback){
-        const chan = this.client.channels.get(channel);
-        chan.subscribe(event, callback);
+//     subscribe(channel, event, callback){
+//         const chan = this.client.channels.get(channel);
+//         chan.subscribe(event, callback);
+//     }
+
+//     history(channel, callback){
+//         const chan = this.client.channels.get(channel);
+//         chan.history(callback);
+//     }
+// }
+
+export const pubsub = (key, channel)=>{
+    var client = new Ably.Realtime(key);
+    var chan = client.channels.get(channel);
+    return {
+        publish: (event, message)=>{        
+            chan.publish(event, message);
+        },
+
+        subscribe: (event, callback)=>{
+            chan.subscribe(event, callback);
+        },
+
+        history: (callback) => {
+            chan.history(callback);
+        }
     }
 }
 
