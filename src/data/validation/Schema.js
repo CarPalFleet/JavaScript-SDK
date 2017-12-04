b8c6c009f2dd7c97ef2c811d84a43164597e8f1d
Nick Chen <ccwukong@gmail.com>
2017-12-04 19:50:08 +0800

    CON-188

--- src/data/validation/Schema.js
+++ src/data/validation/Schema.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 import endpoints from '../Endpoint';
 import camelize from 'camelize';
 
-export const getSchemaAsync = async (service, schema_name)=>{
+export const getSchemaAsync = async (service, schemaName)=>{
     try{
-        const response = await axios.get(endpoints.SCHEMAS.replace('{0}', service).replace('{1}', schema_name));
+        const response = await axios.get(endpoints.SCHEMAS.replace('{0}', service).replace('{1}', schemaName));
         return camelize(response.data.data);
     }catch(e){
         return Promise.reject({statusCode: e.response.status, statusText: e.response.statusText});
