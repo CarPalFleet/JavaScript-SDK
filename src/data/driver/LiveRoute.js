2cfc6acb649d73cdb7c623f3955d6373f401b77b
Dede <winsandymyint@gmail.com>
2017-12-20 11:11:49 +0800

    Update liveRoute params

--- src/data/driver/LiveRoute.js
+++ src/data/driver/LiveRoute.js
@@ -2,26 +2,16 @@ import axios from 'axios';
 import endpoints from '../Endpoint';
 import camelize from 'camelize';
 
-export const sendLiveRouteDataAsync = async ({orderId, addressId, customerId, driverId, 
-    driverFirstName, driverLastName, driverTypes, latitude, longitude, orderRouteType}, token)=>{
+export const sendLiveRouteDataAsync = async (driverInfo, token)=>{
     try{
         const response = await axios({method: 'post',
                                       url: endpoints.DRIVER_LIVE_ROUTES.replace('{0}', driverId),
                                       headers: {'Content-Type': 'application/json'},
-                                      data: {
-                                        orderId,
-                                        addressId,
-                                        customerId,
-                                        driverId,
-                                        driverFirstName,
-                                        driverLastName,
-                                        driverTypes,
-                                        latitude,
-                                        longitude,
-                                        orderRouteType
-                                      }});
+                                      data: driverInfo});
+        console.log("response", response)
         return camelize(response.data.data);
     }catch(e){
+        console.log("Error", e)
         return Promise.reject({statusCode: e.response.status, statusText: e.response.statusText});
     }
 }
