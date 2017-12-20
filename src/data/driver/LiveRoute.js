fa1e09c5dfc74b34b3908b5613dbaa82bff66593
Dede <winsandymyint@gmail.com>
2017-12-20 12:25:50 +0800

    Allow dirver to send token to call liveRoute

--- src/data/driver/LiveRoute.js
+++ src/data/driver/LiveRoute.js
@@ -3,13 +3,12 @@ import endpoints from '../Endpoint';
 import camelize from 'camelize';
 
 export const sendLiveRouteDataAsync = async (driverInfo, token)=>{
-    console.log("************ driverInfo, token *************")
     try{
         const response = await axios({method: 'post',
                                       url: endpoints.DRIVER_LIVE_ROUTES.replace('{0}', driverInfo.driverId),
                                       headers: {
                                         'Content-Type': 'application/json'
-                                        // 'Authorization': token
+                                        'Authorization': token
                                       },
                                       data: driverInfo});
         return camelize(response.data.data);
