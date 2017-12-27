34267466a836613e799d9f5faca53f9a3ed06c90
Dede <winsandymyint@gmail.com>
2017-12-27 11:01:59 +0800

    Rename token to accessToken in Config file Add new transaction customer account credential Update End point of show driver

--- src/data/customer/__test__/search.test.js
+++ src/data/customer/__test__/search.test.js
@@ -5,7 +5,7 @@ import CONFIG from './Config';
 
 test('Testing for search function', async () =>{
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-    const result = getTokenAsync(CONFIG.email, CONFIG.password, CONFIG.clientId, CONFIG.token);
+    const result = getTokenAsync(CONFIG.email, CONFIG.password, CONFIG.clientId, CONFIG.clientSecret);
     const token = await result;
     const response = await searchAsync('mar 1', 'orders,drivers', true, 1, token.accessToken);
     expect(response.orders.total>0).toBe(true);
