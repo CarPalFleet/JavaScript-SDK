02136d612e125952dfc36b7cf66b3778e51bed95
Dede <winsandymyint@gmail.com>
2018-01-05 11:45:33 +0800

    Rename driverId into id in liveRoute

--- src/data/driver/LiveRoute.js
+++ src/data/driver/LiveRoute.js
@@ -2,17 +2,8 @@ import axios from 'axios';
 import endpoints from '../Endpoint';
 import camelize from 'camelize';
 
-export const sendLiveRouteDataAsync = async (payload, token)=>{
+export const sendLiveRouteDataAsync = async (liveRouteObj, token)=>{
     try{
-        let liveRouteObj = {
-          id: payload.driverId,
-          orderId: payload.orderId,
-          addressId: 0,
-          latitude: payload.latitude,
-          longitude: payload.longitude,
-          orderRouteTypeId: 1
-        }
-
         const response = await axios({method: 'post',
                                       url: endpoints.DRIVER_LIVE_ROUTES.replace('{0}', liveRouteObj.driverId),
                                       headers: {
