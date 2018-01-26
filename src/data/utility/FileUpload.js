6697b50044c9cdb167b5f4a170e91b0cca0591a7
Dede <winsandymyint@gmail.com>
2018-01-26 15:57:36 +0800

    test fileupload endpoint with identities

--- src/data/utility/FileUpload.js
+++ src/data/utility/FileUpload.js
@@ -7,17 +7,22 @@ export const fileUploadAsync = async (fileObject, token) => {
   try {
     fileObject = snakeCaseDecorator(fileObject);
     console.log("Token", token);
-    let axiosData = {
-      method: 'POST',
-      url: endpoints.BATCH_FILE_UPLOAD,
-      header: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/x-www-form-urlencoded'
-              },
-      data: {"grouping_spreadsheet": JSON.stringify(fileObject.grouping_spreadsheet)}
-    }
+    // let axiosData = {
+    //   method: 'POST',
+    //   url: endpoints.BATCH_FILE_UPLOAD,
+    //   header: {
+    //             'Authorization': `Bearer ${token}`,
+    //             'Content-Type': 'application/x-www-form-urlencoded'
+    //           },
+    //   data: {"grouping_spreadsheet": JSON.stringify(fileObject.grouping_spreadsheet)}
+    // }
 
-    let response = await axios(axiosData);
+    let response = axios({
+      method: 'GET',
+      url: 'https://api-test.carpal.me/v3/identities',
+    });
+
+    // let response = await axios(axiosData);
     console.log("RES", response);
     return camelize(response);
   } catch (e) {
