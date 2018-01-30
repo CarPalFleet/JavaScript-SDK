5037b9085f9f4ef99083b033937489dc78154080
Nick Chen <ccwukong@gmail.com>
2018-01-30 10:04:04 +0800

    added algo folder

---
+++ src/algo/__test__/Config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+    email: 'supportsg@carpal.me',
+    password: 'Hoofdletter50!',
+    temail: 'nina+fleet@carpal.me',
+    tpassword: 'xs4nin',
+    clientId: '2',
+    token: '8iQwaS8mJLvrW5KrGUrjDZ90bTrwk7frv4ygk7qt',
+    date: '2017-11-01',
+    emptyString: ''
+  }
+  
+  export default CONFIG;
+  
