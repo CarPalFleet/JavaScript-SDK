343d4a707157d8ffa0369d8c7cf8b64e665f7be2
Christopher Histe <chrishiste@gmail.com>
2018-03-09 14:37:19 +0800

    separates unit and integration testing, shows an example on how write integration tests

--- src/data/account/__test__/auth.test.js
+++ src/data/account/__test__/auth.test.js
@@ -1,39 +1,44 @@
-import {getTokenAsync, refreshTokenAsync} from '../Auth';
-import CONFIG from './Config';
+import {getTokenAsync} from '../Auth';
+import config from './config';
 
-test('test for account alpha.test@carpal.me', async () => {
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-  const result = getTokenAsync(
-    CONFIG.temail,
-    CONFIG.tpassword,
-    CONFIG.clientId,
-    CONFIG.clientSecret
-  );
-  const token = await result;
-  expect(token).not.toBeNull();
-});
+describe('Auth integration test', () => {
+  it('verifies that the token data matches the snapshot', async () => {
+    const response = await getTokenAsync(
+      config.temail,
+      config.tpassword,
+      config.clientId,
+      config.clientSecret
+    );
+    expect(response).toMatchSnapshot();
+  });
 
-test('test for account alpha.test@carpal.me with wrong tpassword', async () => {
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-  const result = getTokenAsync(
-    CONFIG.temail,
-    'carpaldemo2',
-    CONFIG.clientId,
-    CONFIG.clientSecret
-  );
-  await expect(result).rejects.toHaveProperty('statusCode', 401);
-});
+  it('test for account alpha.test@carpal.me with wrong tpassword', async () => {
+    try {
+      const response = await getTokenAsync(
+        config.temail,
+        'wrong-password',
+        config.clientId,
+        config.clientSecret
+      );
+      expect(response).toBeNull(); // Should not trigger, only if is there is no exception which is not expected behavior
+    } catch (error) {
+      const expected = {statusCode: 403, statusText: 'Forbidden'};
+      expect(error).toEqual(expected);
+    }
+  });
 
-test('test for refresh token', async () => {
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-  let result = getTokenAsync(
-    CONFIG.temail,
-    CONFIG.tpassword,
-    CONFIG.clientId,
-    CONFIG.clientSecret
-  );
-  let token = await result;
-  result = refreshTokenAsync(token.refreshToken, CONFIG.clientId, CONFIG.token);
-  token = await result;
-  expect(token).toBeTruthy();
+  // TODO convert this old test to a unit and integration
+  // test('test for refresh token', async () => {
+  //   jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+  //   let result = getTokenAsync(
+  //     CONFIG.temail,
+  //     CONFIG.tpassword,
+  //     CONFIG.clientId,
+  //     CONFIG.clientSecret
+  //   );
+  //   let token = await result;
+  //   result = refreshTokenAsync(token.refreshToken, CONFIG.clientId, CONFIG.token);
+  //   token = await result;
+  //   expect(token).toBeTruthy();
+  // });
 });
