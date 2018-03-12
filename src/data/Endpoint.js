4ca47c2168694e54a67f6c2b310860e62f8ef67b
Dede <winsandymyint@gmail.com>
2018-03-12 11:21:24 +0800

    update endpoint url for alpha release

--- src/data/Endpoint.js
+++ src/data/Endpoint.js
@@ -1,6 +1,6 @@
 const urlPrefix =
-  'tes.https://b2gp6sn1o4.execute-api.ap-southeast-1.amazonaws.com/prod';
-const laravelUrlPrefix = 'tes.https://api.carpal.me';
+  'https://b2gp6sn1o4.execute-api.ap-southeast-1.amazonaws.com/prod';
+const laravelUrlPrefix = 'https://api.carpal.me';
 const version = 'v3';
 
 const endpoints = {
