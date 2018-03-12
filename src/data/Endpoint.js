627eee144f70f745df8e663c6a842bc0f1c6bf7e
Dede <winsandymyint@gmail.com>
2018-03-12 11:13:24 +0800

    Update travis yml file

--- src/data/Endpoint.js
+++ src/data/Endpoint.js
@@ -1,6 +1,6 @@
 const urlPrefix =
-  'https://b2gp6sn1o4.execute-api.ap-southeast-1.amazonaws.com/prod';
-const laravelUrlPrefix = 'https://api.carpal.me';
+  'test.https://b2gp6sn1o4.execute-api.ap-southeast-1.amazonaws.com/prod';
+const laravelUrlPrefix = 'test.https://api.carpal.me';
 const version = 'v3';
 
 const endpoints = {
