30494aeb9871a1e97bef60fe35f665bc2f35e984
Maarten Hemmes <maarten@carpal.me>
2018-04-03 22:26:06 +0800

    no message

--- src/data/account/Account.js
+++ src/data/account/Account.js
@@ -86,6 +86,7 @@ export const validateResetPasswordTokenAsync = async (token) => {
  * @param {string} token
  * @param {string} date
  * @return {object} Promise resolve/reject
+ * @deprecated since version 0.1.77
  */
 export const getDriverJobsAsync = async (id, token, date) => {
   try {
@@ -107,6 +108,7 @@ export const getDriverJobsAsync = async (id, token, date) => {
  * @param {string} token
  * @param {string} date
  * @return {object} Promise resolve/reject
+ * @deprecated since version 0.1.77
  */
 export const getDriverLegsAsync = async (id, token, date) => {
   try {
