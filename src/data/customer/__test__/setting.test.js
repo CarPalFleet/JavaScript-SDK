d55b7da8f0527f1a80e36b2ecc5da8dc2794609f
Maarten Hemmes <maarten@carpal.me>
2018-04-11 20:58:11 +0800

    added unit test

--- src/data/customer/__test__/setting.test.js
+++ src/data/customer/__test__/setting.test.js
@@ -1,8 +1,27 @@
 import {getTokenAsync} from '../../account/Auth';
 import {
   getCustomerPreferenceSettingsAsync,
+  getCustomerSettingsAsync,
 } from '../Setting';
 import CONFIG from './Config';
+
+describe('Retrieve Customer settings', () => {
+  it('should response object including customer settings', async () => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+    const result = getTokenAsync(
+      CONFIG.email,
+      CONFIG.password,
+      CONFIG.clientId,
+      CONFIG.clientSecret
+    );
+    const token = await result;
+    const response = getCustomerSettingsAsync(
+      token.accessToken
+    );
+    const settings = await response;
+    expect('data' in settings).toBeTruthy();
+  });
+});
 
 describe('Retrieve whitelabel', () => {
   it('should response object including whitelabel info', async () => {
