3db4eda1be36a11dee7b4d65bea09005d3f2220b
Maarten Hemmes <maarten@carpal.me>
2018-04-12 11:30:18 +0800

    remove unit test

--- src/data/customer/__test__/setting.test.js
+++ src/data/customer/__test__/setting.test.js
@@ -23,25 +23,6 @@ describe('Retrieve Customer settings', () => {
   });
 });
 
-describe('Retrieve whitelabel', () => {
-  it('should response object including whitelabel info', async () => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-    const result = getTokenAsync(
-      CONFIG.email,
-      CONFIG.password,
-      CONFIG.clientId,
-      CONFIG.clientSecret
-    );
-    const token = await result;
-    const response = getCustomerPreferenceSettingsAsync(
-      CONFIG.domain,
-      token.accessToken
-    );
-    const whiteLabel = await response;
-    await expect(whiteLabel).rejects.toHaveProperty('statusCode', 404);
-  });
-});
-
 describe('Retrieve whitelabel with invalid domain', () => {
   it('should get error statusCode', async () => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
