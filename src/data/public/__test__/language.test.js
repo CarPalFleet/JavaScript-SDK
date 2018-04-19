798e15b2efc0e92bf275d105cbb56b3b96300695
Vitalii Danylov <v.danylov@ideamotive.co>
2018-04-20 00:27:19 +0200

    [EB-1409] Tests for getCustomerPublicProfileSettingsAsync

--- src/data/public/__test__/language.test.js
+++ src/data/public/__test__/language.test.js
@@ -1,6 +1,7 @@
 import {getLanguagesAsync} from '../Language';
 
 test('test for languages', async () => {
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
   const result = await getLanguagesAsync();
   expect(result.length).toBeTruthy();
 });
