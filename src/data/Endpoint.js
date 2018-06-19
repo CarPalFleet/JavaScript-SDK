13fd91ad0e6fa16af1fa046a5959e912aa076bb2
Maarten Hemmes <maarten@carpal.me>
2018-06-19 15:35:30 +0800

    change url

--- src/data/Endpoint.js
+++ src/data/Endpoint.js
@@ -1,6 +1,6 @@
 const urlPrefix =
-  'https://b2gp6sn1o4.execute-api.ap-southeast-1.amazonaws.com/pre-test';
-const laravelUrlPrefix = 'https://api-pre-test.carpal.me';
+  'https://b2gp6sn1o4.execute-api.ap-southeast-1.amazonaws.com/staging';
+const laravelUrlPrefix = 'https://api-test.carpal.me';
 const version = 'v3';
 
 const endpoints = {
