56fc8d030ac10fbddb3a6aab1e1c9220db67055f
Maarten Hemmes <maarten@carpal.me>
2018-08-02 14:24:46 +0800

    Update Endpoint.js

--- src/data/Endpoint.js
+++ src/data/Endpoint.js
@@ -1,6 +1,6 @@
 const urlPrefix =
-  'https://b2gp6sn1o4.execute-api.ap-southeast-1.amazonaws.com/alpha';
-const laravelUrlPrefix = 'https://alpha-api.carpal.me';
+  'https://b2gp6sn1o4.execute-api.ap-southeast-1.amazonaws.com/prod';
+const laravelUrlPrefix = 'https://api.carpal.me';
 const version = 'v3';
 
 const endpoints = {
