941eee105e23b3624e4f4cf54f07e6e0014ef924
Maarten Hemmes <maarten@carpal.me>
2018-09-24 09:12:12 +0800

    Update Endpoint.js

--- src/data/Endpoint.js
+++ src/data/Endpoint.js
@@ -1,5 +1,5 @@
 const urlPrefix =
-  'https://b2gp6sn1o4.execute-api.ap-southeast-1.amazonaws.com/dev';
+  'https://b2gp6sn1o4.execute-api.ap-southeast-1.amazonaws.com/staging';
 const laravelUrlPrefix = 'https://api-test.carpal.me';
 const version = 'v3';
 
