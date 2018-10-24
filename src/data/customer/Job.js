a31ab3de52cb48b5617e17fa0feb47a2197ebf1b
Maarten Hemmes <maarten@carpal.me>
2018-10-25 06:14:04 +0800

    Fixes getjobs

--- src/data/customer/Job.js
+++ src/data/customer/Job.js
@@ -10,6 +10,29 @@ import {
   apiResponseErrorHandler,
 } from '../utility/Util';
 
+
+/**
+ * Returns jobs created by the customer
+ * @param {object} filterObject # {pickupDateStart, pickupDateEnd, transactionGroupIds, jobStatusIds, includes, page, limit}
+ * @param {string} token
+ * @return {object} Promise resolve/reject
+ */
+
+export const getJobsAsync = async (token, filterObject = {}) => {
+  let paramString = convertObjectIntoURLString(filterObject);
+  try {
+    const jobs = await axios({
+      method: 'GET',
+      url: `${endpoints.JOB.replace('{0}', paramString)}`,
+      headers: { Authorization: `bearer ${token}` },
+    });
+
+    return camelize(jobs.data);
+  } catch (e) {
+    return apiResponseErrorHandler(e);
+  }
+};
+
 /**
  * Get Job Detail
  * @param {int} jobId
