6bd16da1aa0bf3e7fa17c9e60fc47ec00a6e186b
paul santos <paul@carpal.me>
2019-11-12 15:48:25 +0800

    Update api call

--- src/data/customer/Job.js
+++ src/data/customer/Job.js
@@ -403,10 +403,10 @@ export const getProofDownload = async ({ jobId, locationId, token }) => {
   try {
     const response = await axios({
       method: 'GET',
-      url: `${endpoints.JOB.replace(
+      url: `${endpoints.API_V3.JOB.replace(
         '{0}',
         jobId
-      )}/location/${locationId}/proof/download}`,
+      )}/location/${locationId}/proof/download`,
       headers: { Authorization: `Bearer ${token}` },
     });
     return camelize(response.data);
